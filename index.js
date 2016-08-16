581b6faf4abab8c0d50ae2b700544e9007a93c58
dsoffiantini <dsoffiantini@gmail.com>
2016-08-16 12:58:28 -0600

    8.16

--- index.js
+++ index.js
@@ -19,3 +19,16 @@ var countBits = function(n) {
   return count;
 
 };
+
+
+//move all zeroes to end of array
+
+var moveZeros = function (arr) {
+  for(var i = arr.length; i--;) {
+     if(arr[i] === 0) {
+        arr.splice(i, 1);
+        arr.push(0);
+      }
+  }
+  return arr;
+}
